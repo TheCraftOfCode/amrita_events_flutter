087909a42adbe4bee3e862d0e93e866f2dff8341
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2022-05-25 17:44:13 +0530

    refactored main

--- lib/screens/starred_events_page.dart
+++ lib/screens/starred_events_page.dart
@@ -2,12 +2,8 @@ import 'package:amrita_events_flutter/widgets/custom_sliver_widget.dart';
 import 'package:amrita_events_flutter/widgets/starred_card.dart';
 import 'package:amrita_events_flutter/widgets/top_bar_widget.dart';
 import 'package:flutter/material.dart';
-
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:amrita_events_flutter/utils/colors.dart' as colors;
 
-import '../onboarding.dart';
 
 class Starred extends StatefulWidget {
   const Starred({Key? key}) : super(key: key);
@@ -21,15 +17,15 @@ class _StarredState extends State<Starred> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: colors.scaffoldColor,
-      body: CustomSliverView(
+      body: const CustomSliverView(
         columnList: [
           TopBarWidget(icon: Icons.star_border_sharp, title: "Starred"),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: EdgeInsets.all(8.0),
             child:  StarCard(event_name: "event_name", time: "time", date: "date")
           ) ,
           Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(8.0),
               child:  StarCard(event_name: "event_name", time: "time", date: "date")
           ) ],
       ),
--- lib/screens/the_main.dart
+++ lib/screens/the_main.dart
@@ -1,14 +1,10 @@
 import 'package:amrita_events_flutter/screens/events_home.dart';
 import 'package:amrita_events_flutter/screens/notifications.dart';
 import 'package:amrita_events_flutter/screens/profile.dart';
-import 'package:amrita_events_flutter/screens/settings.dart';
-import 'package:amrita_events_flutter/screens/starred.dart';
-import 'package:amrita_events_flutter/widgets/custom_sliver_widget.dart';
-import 'package:amrita_events_flutter/widgets/top_bar_no_search_widget.dart';
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:amrita_events_flutter/screens/settings_page.dart';
+import 'package:amrita_events_flutter/screens/starred_events_page.dart';
 import 'package:amrita_events_flutter/utils/colors.dart' as colors;
+import 'package:flutter/material.dart';
 
 class TheMain extends StatefulWidget {
   const TheMain({Key? key}) : super(key: key);
@@ -18,39 +14,46 @@ class TheMain extends StatefulWidget {
 }
 
 class _TheMainState extends State<TheMain> {
-  int currentIndex =0;
-  final Screens =[
-     EventsHome(),
-     Starred(),
-    Profile(),
-    Settings(),
-    Notifications()
-
+  int currentIndex = 0;
+  final screens = [
+    EventsHome(),
+    const Starred(),
+    const Profile(),
+    const Settings(),
+    const Notifications()
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: colors.scaffoldColor,
-      body: Screens[currentIndex],
+      body: screens[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-currentIndex: currentIndex,
-        onTap: (index)=> setState(()=> currentIndex=index),
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.white70,
         showUnselectedLabels: true,
         items: [
           BottomNavigationBarItem(
-
-            backgroundColor: colors.scaffoldColor,
-              icon: Icon(Icons.play_arrow_outlined), label: 'Events'),
-          BottomNavigationBarItem(backgroundColor: colors.scaffoldColor,
-              icon: Icon(Icons.star_border_sharp), label: 'Starred'),
-          BottomNavigationBarItem(backgroundColor: colors.scaffoldColor,
-              icon: Icon(Icons.tag_faces_outlined), label: 'Profile'),
-          BottomNavigationBarItem(backgroundColor: colors.scaffoldColor,
-              icon: Icon(Icons.settings), label: 'Settings'),
-          BottomNavigationBarItem(backgroundColor: colors.scaffoldColor,
-              icon: Icon(Icons.notifications_none_outlined),
+              backgroundColor: colors.scaffoldColor,
+              icon: const Icon(Icons.play_arrow_outlined),
+              label: 'Events'),
+          BottomNavigationBarItem(
+              backgroundColor: colors.scaffoldColor,
+              icon: const Icon(Icons.star_border_sharp),
+              label: 'Starred'),
+          BottomNavigationBarItem(
+              backgroundColor: colors.scaffoldColor,
+              icon: const Icon(Icons.tag_faces_outlined),
+              label: 'Profile'),
+          BottomNavigationBarItem(
+              backgroundColor: colors.scaffoldColor,
+              icon: const Icon(Icons.settings),
+              label: 'Settings'),
+          BottomNavigationBarItem(
+              backgroundColor: colors.scaffoldColor,
+              icon: const Icon(Icons.notifications_none_outlined),
               label: 'Notifications')
         ],
       ),
