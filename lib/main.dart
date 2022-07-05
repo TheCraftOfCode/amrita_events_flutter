659e5806523dbba396b4546c011b273eeec2c0de
sooryasrajan <soorya.s27@gmail.com>
2022-07-05 20:34:44 +0530

    Added pending data

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:amrita_events_flutter/screens/the_main.dart';
 import 'package:amrita_events_flutter/widgets/load_valid_page_widget.dart';
 import 'package:flutter/material.dart';
 
-//TODO: Add login page for admins (only login)
-//TODO: Fix overflows
 //TODO: Add admin page
+//TODO: Completed all events and notification pages
+//TODO: Add admin page, profile page and all other sorts of pending page
 
 void main() {
   runApp(const MaterialApp(
