8f687aa4e4284f44c454286f813015292b085d80
sooryasrajan <soorya.s27@gmail.com>
2022-07-08 16:27:22 +0530

    Adding basic widgets to build admin page

--- lib/screens/admin/admin_console.dart
+++ lib/screens/admin/admin_console.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:amrita_events_flutter/utils/colors.dart' as colors;
+import '../../widgets/custom_sliver_widget.dart';
+import '../../widgets/top_bar_no_search_widget.dart';
 
 class AdminConsole extends StatelessWidget {
   const AdminConsole({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: colors.scaffoldColor,
+      body: CustomSliverView(
+        columnList: [
+          const TopBarWidgetNoSearch(
+              icon: Icons.settings_outlined, title: "Admin"),
+            Expanded(
+              child: Column(
+                children: const [
+                  Expanded(child: Card(
+                    child: Text("TILE"),
+                  )),
+                  Expanded(child: Card(
+                    child: Text("TILE"),
+                  )),
+                  Expanded(child: Card(
+                    child: Text("TILE"),
+                  )),
+                  Expanded(child: Card(
+                    child: Text("TILE"),
+                  )),
+                ],
+              ),
+            )
+        ],
+      ),
+    );
   }
 }
