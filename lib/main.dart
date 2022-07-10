b79e9f9481bb5fb27baa2040eab8c2010141886a
sooryasrajan <soorya.s27@gmail.com>
2022-07-10 23:39:19 +0530

    Configured firebase using Firebase CLI Added pubspec dependencies for firebase console and firebase messaging Added local notifications plug in to display notifications in foreground Also configured notification channel and icon in manifest

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,119 @@
 import 'package:amrita_events_flutter/screens/greeting_page.dart';
 import 'package:amrita_events_flutter/screens/the_main.dart';
 import 'package:amrita_events_flutter/widgets/load_valid_page_widget.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'firebase_options.dart';
 
-//TODO: Add admin page
-//TODO: Completed all events and notification pages
-//TODO: Add admin page, profile page and all other sorts of pending page
+//TODO: Configure APNs for iOS notification
+//reference: https://firebase.flutter.dev/docs/messaging/apple-integration/
 
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: LoadValidPageWidget(GreetingPage(), TheMain()),
-  ));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  Future<void> initNotification() async {
+    print("INIT");
+    final fcmToken = await FirebaseMessaging.instance.getToken();
+    print(fcmToken);
+    final FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'amrita_events_notification_channel', // id
+      'Amrita Events Notifications', // title
+      description: 'This channel is used for amrita events notifications.',
+      // description
+      importance: Importance.max,
+    );
+
+    const IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+    );
+
+    messaging.subscribeToTopic('main');
+
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    var initializationSettingsAndroid =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    FlutterLocalNotificationsPlugin().initialize(initializationSettings);
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channelDescription: channel.description,
+                icon: android.smallIcon,
+              ),
+              iOS: const IOSNotificationDetails(
+                  presentAlert: true,  // Present an alert when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+                  presentSound: true,  // Play a sound when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+              )
+            ));
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print("_messaging onMessageOpenedApp: $message");
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initNotification();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoadValidPageWidget(GreetingPage(), TheMain()),
+    );
+  }
 }
