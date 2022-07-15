117736dd82028d1741d91ef44557d1976126097a
sooryasrajan <soorya.s27@gmail.com>
2022-07-15 13:12:15 +0530

    UI fix for buttons in profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -18,15 +18,15 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-
-  final GlobalKey<ProfileCardState> _profileCardKey = GlobalKey<ProfileCardState>();
+  final GlobalKey<ProfileCardState> _profileCardKey =
+      GlobalKey<ProfileCardState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: colors.scaffoldColor,
       body: RefreshIndicator(
-        onRefresh: () async{
+        onRefresh: () async {
           await _profileCardKey.currentState!.getUserData();
         },
         child: CustomSliverView(
@@ -38,7 +38,9 @@ class _ProfileState extends State<Profile> {
               child: Container(),
               flex: 2,
             ),
-            ProfileCard(key: _profileCardKey,),
+            ProfileCard(
+              key: _profileCardKey,
+            ),
             Expanded(
               child: Container(),
               flex: 1,
@@ -51,57 +53,57 @@ class _ProfileState extends State<Profile> {
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.only(right: 4),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          displayDialog(context, "Yes", "No", () {
-                            clearAllData();
-                            // showToast("Signed out successfully!");
-                            Navigator.of(context).pushAndRemoveUntil(
-                                MaterialPageRoute(
-                                    builder: (context) => const GreetingPage()),
-                                (Route<dynamic> route) => false);
-                          }, "Are you sure you want to sign out?",
-                              "You will be signed out and all data will be lost");
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 5.0, vertical: 7.0),
+                      child: SizedBox(
+                        height: 60,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            displayDialog(context, "Yes", "No", () {
+                              clearAllData();
+                              // showToast("Signed out successfully!");
+                              Navigator.of(context).pushAndRemoveUntil(
+                                  MaterialPageRoute(
+                                      builder: (context) => const GreetingPage()),
+                                  (Route<dynamic> route) => false);
+                            }, "Are you sure you want to sign out?",
+                                "You will be signed out and all data will be lost");
+                          },
                           child: Text(
-                            "SIGN OUT",
+                            "SIGNOUT",
+                            textAlign: TextAlign.center,
                             style: GoogleFonts.raleway(
                                 fontSize: 16,
                                 color: colors.headingTextColor,
                                 fontWeight: FontWeight.bold),
                           ),
+                          style: ElevatedButton.styleFrom(
+                              primary: colors.accentColor),
                         ),
-                        style: ElevatedButton.styleFrom(
-                            primary: colors.accentColor),
                       ),
                     ),
                   ),
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.only(left: 4),
-                      child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (BuildContext context) {
-                              return const ChangePassword();
-                            }));
-                          },
-                          style: ElevatedButton.styleFrom(
-                              primary: colors.accentColor),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 0.0, vertical: 10.0),
+                      child: SizedBox(
+                        height: 60,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                                return const ChangePassword();
+                              }));
+                            },
+                            style: ElevatedButton.styleFrom(
+                                primary: colors.accentColor),
                             child: Text(
                               "CHANGE PASSWORD",
+                              textAlign: TextAlign.center,
                               style: GoogleFonts.raleway(
                                   fontSize: 14.8,
                                   color: colors.primaryTextColor,
                                   fontWeight: FontWeight.bold),
-                            ),
-                          )),
+                            )),
+                      ),
                     ),
                   ),
                 ],
